ce6f8911a99cb5662f510b1a994185913649f224
DarDarDinks22 <indracrow0@gmail.com>
2025-07-13 17:50:52 +0700

    add Frame

--- src/app/Component/Organism/homepage.tsx
+++ src/app/Component/Organism/homepage.tsx
@@ -1,9 +1,11 @@
+import Frame from "../Atom/Frame";
 import Robot from "../Atom/Robot";
 import HeroText from "../Molecule/HeroText";
 
  const Homepage = () =>{
     return(
         <div className="flex justify-center items-center px-24 text-3xl h-screen w-full">
+            <Frame/>
             <HeroText/>
             <Robot/>
         </div>
