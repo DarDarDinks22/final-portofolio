519b2134dc3a0762c7a325d6758bca16ea5b2252
DarDarDinks22 <indracrow0@gmail.com>
2025-07-20 23:52:19 +0700

    gsap

--- src/app/Component/Molecule/TechList.tsx
+++ src/app/Component/Molecule/TechList.tsx
@@ -1,6 +1,18 @@
 import { TechItem } from "../Atom/TechListItem";
+import {ScrollTrigger} from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 const TechList = () => {
+
+    const gsapScrollTrigger = () =>{
+        const scrollRef = useRef();
+
+        useGSAP (() => {
+
+        },[])
+    }
+
     return (
         <section>
             <div className="">
