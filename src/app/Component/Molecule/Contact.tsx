ac4693c2a97eb9ab011edf973d495bf4dd16c491
DarDarDinks22 <indracrow0@gmail.com>
2025-08-15 22:50:58 +0700

    WA

--- src/app/Component/Molecule/Contact.tsx
+++ src/app/Component/Molecule/Contact.tsx
@@ -1,6 +1,17 @@
-const Contact = () => {
-    return (
+import Link from "next/link";
 
+const Contact = () => {
+
+    const phoneNumber = "6281357836633"
+
+    return (
+        <div className="">
+            <a 
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://wa.me/${phoneNumber}`}> 
+            </a>
+        </div>
     );
 }
 
