76312b26b8d291c0ab92cff2ef48ea5dd73cfb34
DarDarDinks22 <indracrow0@gmail.com>
2025-09-23 21:04:51 +0700

    fix1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import NavBar from "./Component/Organism/NavBar";
 import { Tektur } from "next/font/google";
 import Homepage from "./Component/Organism/homepage";
-import Project from "./Component/Molecule/project";
+import Project from "./Component/Molecule/Project";
 import About from "./Component/Molecule/About";
 import TechList from "./Component/Molecule/TechList";
 import Contact from "./Component/Molecule/Contact";
