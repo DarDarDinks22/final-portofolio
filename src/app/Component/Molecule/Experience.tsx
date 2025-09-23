20064b69ffa532529a32c62a00abfa63d90436bc
DarDarDinks22 <indracrow0@gmail.com>
2025-09-23 20:49:04 +0700

    miss2

--- src/app/Component/Molecule/Experience.tsx
+++ src/app/Component/Molecule/Experience.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import ScrollTrigger from "gsap/all";
