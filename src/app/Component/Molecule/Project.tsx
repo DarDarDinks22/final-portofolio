53f6d75a081951c4856a298f8e73187621f013ab
DarDarDinks22 <indracrow0@gmail.com>
2025-10-08 16:19:14 +0700

    removed ghost hover due to it looks bad

--- src/app/Component/Molecule/Project.tsx
+++ src/app/Component/Molecule/Project.tsx
@@ -11,29 +11,6 @@ const Project = () => {
 
     const scrollRef = useRef<HTMLDivElement | null> (null);
     const itemRefs = useRef<HTMLDivElement[]>([]);
-    const ghostRef = useRef<HTMLDivElement | null> (null);
-
-    const handleMouseMove = (e: React.MouseEvent) => {
-        gsap.to(ghostRef.current, {
-            x: e.clientX + 20, 
-            y: e.clientY + 20, 
-            duration: 0.2, 
-            ease: "power3.out"
-        });
-    };
-
-    const handleMouseEnter = (imgURL : string) => {
-        if (ghostRef.current) {
-            ghostRef.current.style.backgroundImage = `url(${imgURL})`;
-            ghostRef.current.style.opacity = "1";
-        }
-    }
-
-    const handleMouseLeave = () => {
-        if (ghostRef.current) {
-            ghostRef.current.style.opacity = "0"
-        }
-    }
 
     useGSAP(() => {
         itemRefs.current.forEach((el) => {
@@ -63,26 +40,15 @@ const Project = () => {
     return(
     <section id="project">
         <div 
-            className="relative text-center w-full h-2/4"
-            onMouseMove={handleMouseMove}>
+            className="relative text-center w-full h-2/4">
             <h1 className="pb-2 text-5xl">Check out my Project!</h1>
             <h2 className="pb-8">Click them to see the source code!</h2>
-                <div
-                    ref={ghostRef}
-                    className="pointer-events-none fixed top-0 left-0 w-64 h-40 z-50 opacity-0 bg-cover bg-center bg-no-repeat rounded-xl border border-white/10 shadow-xl"
-                    style={{
-                    mixBlendMode: "screen",
-                    transition: "opacity 0.2s ease",
-                    }}
-            />
                 <div    className="flex  flex-wrap gap-4 justify-center items-center" 
                         ref={scrollRef}>
 
                     {ProjectItems.map((project, index) => (
                         <div
                             key={index}
-                            onMouseEnter={() => handleMouseEnter(project.imgURL)}
-                            onMouseLeave={handleMouseLeave}
                         >
                         <Card 
                             key={index}
